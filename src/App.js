17830c5c05ffa2d578c2e29d63f871e6302990ed
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2019-01-09 21:34:36 -0800

    added person component and props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //   <h1>Hi, I'm a react app</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
+    return (
+      <div className="App">
+      <h1>Hi, I'm a react app</h1>
+      <Person name="Max" age="28"/>
+      <Person name="Laura" age="30"/>
+      <Person name="Stephanie" age="23"/>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
